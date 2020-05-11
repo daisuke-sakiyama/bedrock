b56b9bed000e6b3e596f32400e5d70cd67b68d1e
daisuke-sakiyama <sakiyama009@gmail.com>
2020-05-11 20:27:46 +0900

    update

--- app/bedrock/web/app/themes/kokusai/resources/views/partials/head.blade.php
+++ app/bedrock/web/app/themes/kokusai/resources/views/partials/head.blade.php
@@ -2,7 +2,5 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  @include('partials/_inc_/views/config/meta')
   @php wp_head() @endphp
-  @include('partials/_inc_/views/config/script')
 </head>
--- app/bedrock/web/app/themes/kokusai/resources/views/partials/template-parts/loop/text.php
+++
@@ -1,9 +0,0 @@
-<?php query_posts('&posts_per_page=1'); ?>
-<?php
-  if (have_posts()) :
-    while (have_posts()) :
-      the_post();
-      the_content();
-    endwhile;
-  endif;
-?>
